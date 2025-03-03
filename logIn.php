cdb8aaa4a5065fb38fa6a2ac72c401cb6d9c80f1
Ruby Friedman <rubyfriedman@Rubys-MacBook-Air-2.local>
2025-03-02 20:40:28 -0500

    login changes issue-#80

--- logIn.php
+++ logIn.php
@@ -29,19 +29,33 @@
     </div>
     <?php
     include 'config.php';
-    $userid = $_POST['userid'];
-    $password = $_POST['password'];
-    $sql = "SELECT * FROM users WHERE username = '$userid' AND password = '$password'";
-    $result = mysqli_query($db, $sql) or die(mysqli_error($db));
-    $num = mysqli_fetch_array($result);
-    
-    if($num > 0) {
-     echo "Login Success";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $userid = $_POST['userid'];
+      $password1 = $_POST['password'];
+
+      $sql = "SELECT password FROM users WHERE username = ?";
+      $stmt = mysqli_prepare($conn, $sql);
+    // Bind the values for username and password that the user entered to the
+    // statement AS STRINGS (that is what "ss" means). In other words, the
+    // user input is strictly interpreted by the server as data and not as
+    // porgram code part of the SQL statement.
+    mysqli_stmt_bind_param($stmt, "s", $userid);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+      mysqli_stmt_bind_result($stmt, $hashed_password);
+      mysqli_stmt_fetch($stmt);
+      if (password_verify($password1, $hashed_password)) {
+        echo "Login Success";
+      }
+      else {
+        echo "Wrong User id or password";
+      }
     }
     else {
-     echo "Wrong User id or password";
+      echo "Wrong User id or password";
     }
-
+  }
   ?>
   </body>
 
