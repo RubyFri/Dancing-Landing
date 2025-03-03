d017a09cdee5836ee449691c8500cb53c35627bc
Ruby Friedman <rubyfriedman@Rubys-MacBook-Air-2.local>
2025-03-02 19:45:21 -0500

    testing basic login (no cookie issue-#80

--- logIn.php
+++ logIn.php
@@ -9,7 +9,40 @@
     <link rel="stylesheet" href="stylesheet.css">
   </head>
   <body>
+  <div id="form">
+      <h1>LOG IN</h1>
+      <form name="form" action="" method="POST">
+        <p>
+          <label>Username: </label>
+          <input type="text" id="user" name="userid"/>
+        </p>
 
+        <p>
+          <label>Password: </label>
+          <input type="password" id="pass" name="password" />
+        </p>
+        <p>
+        <p>
+          <input type="submit" id="button" value="Log In" />
+        </p>
+      </form>
+    </div>
+    <?php
+    include 'config.php';
+    $userid = $_POST['userid'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM users WHERE username = '$userid' AND password = '$password'";
+    $result = mysqli_query($db, $sql) or die(mysqli_error($db));
+    $num = mysqli_fetch_array($result);
+    
+    if($num > 0) {
+     echo "Login Success";
+    }
+    else {
+     echo "Wrong User id or password";
+    }
+
+  ?>
   </body>
 
   </html>
